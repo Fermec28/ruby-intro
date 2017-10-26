07ea931e135c97ed3608a1385d8fb931d9f4b528
Fermec28 <fermed28@gmail.com>
2017-10-25 23:07:23 -0500

    Solucion reto de las naranjas

--- challenges/OOP/orange-tree/source/orange_tree.rb
+++ challenges/OOP/orange-tree/source/orange_tree.rb
@@ -4,25 +4,49 @@ end
 
 class OrangeTree
   # Envejeze el arbol
+  attr_reader :age, :height
+  def initialize
+    @age=0
+    @oranges=[]
+    @height=0
+    @dead=false
+
+  end
   def age!
+    @age += 1
+    @height += 0.16
+    if @age>=3
+      (rand(250)+1).times do
+        @oranges << Orange.new(rand(15)+1)
+      end
+    end
+    @dead=true if @age>25
+
+
   end
 
   # Devuelve true si hay naranjas en el arbol, false si no
   def any_oranges?
+      @oranges.any?
   end
 
+  def dead?
+    @dead
+  end
   # Devuelve una naranja si hay una
   # Dispara un NoOrangesError de lo contrario
   def pick_an_orange!
-    raise NoOrangesError, "This tree has no oranges" unless self.any_oranges?
-
+    raise NoOrangesError,  "This tree has no oranges" unless self.any_oranges?
+    @oranges.pop
     # Logica para sacar naranjas va aca
-  end
-end
+  end #end pick_an_orange!
+end ## end class OrangeTree
 
 class Orange
   # Crea una naranja con determinado +diameter+
+ attr_reader :diameter
   def initialize(diameter)
+    @diameter=diameter
   end
 end
 
@@ -32,19 +56,21 @@ end
 tree = OrangeTree.new
 
 tree.age! until tree.any_oranges?
-
 puts "El arbol tiene #{tree.age} años y mide #{tree.height} metros"
 
 until tree.dead?
+
   basket = []
 
   # Pone las naranjas en la canasta
+
   while tree.any_oranges?
     basket << tree.pick_an_orange!
   end
 
 
   avg_diameter = basket.map { |orange| orange.diameter}
+
   avg_diameter = avg_diameter.reduce(:+) / basket.length
   puts "Reposte Año #{tree.age}"
   puts "Altura arbol: #{tree.height} metros"
@@ -55,6 +81,4 @@ until tree.dead?
   #
   tree.age!
 end
-
 puts "Miercoles, se murio el arbol!"
-
